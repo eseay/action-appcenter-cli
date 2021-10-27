2834629b830c40daf9bf349ecb41c26aab237335
Mitch Ware <mitchare@gmail.com>
2021-10-27 14:10:18 -0400

    Add owner_name as explicit input arg.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -l
-appcenter distribute release --app $APP_NAME --file $FILE_PATH --group $GROUP_NAME --silent --debug --token $API_TOKEN
+appcenter distribute release --app $OWNER_NAME/$APP_NAME --file $FILE_PATH --group $GROUP_NAME --silent --debug --token $API_TOKEN
