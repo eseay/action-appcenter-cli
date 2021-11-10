f4036e1f4391f651432d816687c01a3f25c4e2cb
Mitch Ware <mitchare@gmail.com>
2021-11-10 12:30:02 -0500

    Update CLI script to check FILE_PATH existence

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,5 @@
 #!/bin/sh -l
+echo "File path: $FILE_PATH"
+[ ! -f $FILE_PATH ] && { echo "File doesn't exist."; exit 1; }
+
 appcenter distribute release --app "$OWNER_NAME/$APP_NAME" --file "$FILE_PATH" --group "$GROUP_NAME" --silent --debug --token $API_TOKEN
